7f1502fb638248ee2a41e6468d74f4a3b938aa76
Osaretin Igbinobaro <igbinobaroosaretin@gmail.com>
2022-03-03 18:58:59 +0000

    solved the queryString issue

--- pages/product/[productId].js
+++ pages/product/[productId].js
@@ -1,10 +1,15 @@
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 import Viewproduct from "../../components/Viewproduct";
 
-const Product = () => {
-  const router = useRouter();
-  const { productId } = router.query;
+export const getServerSideProps = (context) => {
+  const productId = context.params.productId;
+  return {
+    props: { productId },
+  };
+};
 
+const Product = ({ productId }) => {
   return <Viewproduct productId={productId} />;
 };
 
